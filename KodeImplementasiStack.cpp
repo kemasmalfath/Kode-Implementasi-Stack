293bc5e7a752455ba3ced994eab2567636e0d5e6
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2024-05-29 19:50:53 +0700

    Update KodeImplementasiStack.cpp
    membuat kodingan no 1 parsing string infix,yang terdiir atas variabel jenisOperasi dan KonversiInfix

--- KodeImplementasiStack.cpp
+++ KodeImplementasiStack.cpp
@@ -5,3 +5,56 @@
 #include <algorithm>
 #include <stack>
 using namespace std;
+
+bool JenisOperasi(char A); 
+
+
+vector<string> KonversiInfix(string str);
+
+bool JenisOperasi(char A) {
+    return (A == '*' or A == '/' or A == '+' or A == '-' or A == '%');
+}
+
+
+
+
+
+
+vector<string> KonversiInfix(string str) {
+    vector<string> infix;
+string digit; 
+
+for (size_t i = 0; i < str.size(); i++) {
+    char currentChar = str[i]; 
+
+    if (isspace(currentChar)) {
+        continue;
+    }
+    else if (isdigit(currentChar)) {
+        digit += currentChar;
+    } 
+    else if (currentChar == '-' && (i == 0 || JenisOperasi(str[i - 1]) || str[i - 1] == '(')) {
+        if (str[i + 1] == '(') {
+            infix.push_back("-1");
+            infix.push_back("*");
+        } else if (isdigit(str[i + 1])) {
+            digit += currentChar;
+        } else {
+            infix.push_back("-1");
+            infix.push_back("*");
+        }
+    } 
+    else {
+        if (!digit.empty()) {
+            infix.push_back(digit);
+            digit.clear();
+        }
+        infix.push_back(string(1, currentChar));
+    }
+}
+
+if (!digit.empty()) {
+    infix.push_back(digit);
+}
+return infix;
+}
