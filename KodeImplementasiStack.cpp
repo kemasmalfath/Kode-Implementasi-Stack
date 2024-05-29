3585f7a1d54f84ea1e85ad96281658c69ddd3ee5
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2024-05-29 21:09:32 +0700

    Update KodeImplementasiStack.cpp

--- KodeImplementasiStack.cpp
+++ KodeImplementasiStack.cpp
@@ -5,3 +5,10 @@
 #include <algorithm>
 #include <stack>
 using namespace std;
+
+bool JenisOperasi(char A);
+vector<string> KonversiInfix(string str);
+
+bool JenisOperasi(char A) {
+    return (A == '*' or A == '/' or A == '+' or A == '-' or A == '%');
+}
